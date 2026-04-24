c232e0d4dae97a0aa8ec62129b1976182cf7d291
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2026-04-24 14:30:50 +0900

    feat : 요청 성공, 실패 상수 추가

--- src/main/java/com/example/assignment/domain/type/FailedType.java
+++ src/main/java/com/example/assignment/domain/type/FailedType.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum FailedType {
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+  ACCESS_DENIED(HttpStatus.FORBIDDEN, "해당 작업에 대한 권한이 없습니다."),
+  COURSE_IS_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 등록한 강의 입니다."),
   ;
 
   private final HttpStatus status;
