5638bd45e951aec2c242d7511fc53b7dbb2b9d61
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2026-04-25 15:44:20 +0900

    feat : 요청 성공 상수 추가

--- src/main/java/com/example/assignment/domain/type/SuccessType.java
+++ src/main/java/com/example/assignment/domain/type/SuccessType.java
@@ -15,6 +15,7 @@ public enum SuccessType {
   SUCCESS_INQUIRY_ENROLLMENTS(HttpStatus.OK, "내 수강 신청 목록을 성공적으로 조회하였습니다."),
   SUCCESS_CANCEL_ENROLLMENT(HttpStatus.OK, "신청한 수강을 성공적으로 취소하였습니다."),
   SUCCESS_UPDATE_COURSE_STATUS(HttpStatus.OK, "강의 상태를 성공적으로 변경하였습니다."),
+  SUCCESS_INQUIRY_COURSE_ENROLLMENTS(HttpStatus.OK, "수강생 목록을 조회했습니다."),
   ;
 
   private final HttpStatus status;
