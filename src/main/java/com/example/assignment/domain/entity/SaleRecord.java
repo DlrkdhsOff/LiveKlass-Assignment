cf9161e733533d971d352abb9cf36f3ade5819e2
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2026-04-26 19:17:57 +0900

    feat : 결제 정보 및 결제 취소 정보를 저장할 Entity 클래스 구현

---
+++ src/main/java/com/example/assignment/domain/entity/SaleRecord.java
@@ -0,0 +1,39 @@
+package com.example.assignment.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleRecord extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long saleRecordId;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "enrollment_id", nullable = false)
+  private Enrollment enrollment;
+
+  private Long amount;
+
+  public static SaleRecord toEntity(Enrollment enrollment) {
+    return SaleRecord.builder()
+        .enrollment(enrollment)
+        .amount(enrollment.getCourse().getAmount())
+        .build();
+  }
+}
