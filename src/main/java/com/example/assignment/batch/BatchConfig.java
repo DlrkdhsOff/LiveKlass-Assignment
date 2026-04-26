d5836d3cb8a795ebd6f50aa25e7d107a3b5dd51c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2026-04-26 14:12:50 +0900

    feat : Spring Batch Job 처리를 위한 클래스 구현

---
+++ src/main/java/com/example/assignment/batch/BatchConfig.java
@@ -0,0 +1,41 @@
+package com.example.assignment.batch;
+
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BatchConfig {
+
+  private final JobLauncher jobLauncher;
+  private final Job cancelJob;
+
+  /**
+   * 미결제 자동 취소 배치
+   * 매일 자정에 실행
+   * 강의 시작 하루 전까지 결제하지 않은 PENDING 건을 자동 취소
+   */
+  @Scheduled(cron = "0 0 0 * * *")
+  public void cancelUnpaidBatch() {
+    try {
+      JobParameters jobParameters = new JobParametersBuilder()
+          .addLocalDateTime("executeTime", LocalDateTime.now())
+          .toJobParameters();
+
+      jobLauncher.run(cancelJob, jobParameters);
+      log.info("[배치] 미결제 자동 취소 배치 실행 완료");
+
+    } catch (Exception e) {
+      log.error("[배치] 미결제 자동 취소 배치 실행 실패", e);
+    }
+  }
+
+}
---
+++ src/main/java/com/example/assignment/batch/job/DailyJobConfig.java
@@ -0,0 +1,31 @@
+package com.example.assignment.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DailyJobConfig  {
+
+  private final JobRepository jobRepository;
+  private final Step cancelUnpaidStep;
+  private final Step cancelWaitlistStep;
+
+  /**
+   * 매일 자정 실행되는 수강신청 자동 취소 Job
+   * 1. cancelUnpaidStep  : 강의 시작 하루 전까지 미결제(PENDING) 건 자동 취소
+   * 2. cancelWaitlistStep : 강의 시작 3일 전까지 대기(WAITLISTED) 건 자동 취소
+   */
+  @Bean
+  public Job cancelJob() {
+    return new JobBuilder("dailyCancelEnrollmentJob", jobRepository)
+        .start(cancelUnpaidStep)
+        .next(cancelWaitlistStep)
+        .build();
+  }
+}
